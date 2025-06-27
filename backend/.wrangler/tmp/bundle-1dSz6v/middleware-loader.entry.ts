5ae0cd2247aadaae1eb85ea265e3013b480fe03a
lcfsgtc <lcfsgtc@126.com>
2025-06-27 12:51:11 +0800

    1

--- backend/.wrangler/tmp/bundle-1dSz6v/middleware-loader.entry.ts
+++ backend/.wrangler/tmp/bundle-1dSz6v/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-7vCcFv\\middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-1dSz6v\\middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "C:\\Users\\lcfsg\\AppData\\Roaming\\nvm\\v24.0.1\\node_modules\\wrangler\\templates\\middleware\\common.ts";
-import type { WorkerEntrypointConstructor } from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-7vCcFv\\middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-1dSz6v\\middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-7vCcFv\\middleware-insertion-facade.js";
+export * from "D:\\code\\GitProjects\\clever-accounting-app\\backend\\.wrangler\\tmp\\bundle-1dSz6v\\middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
